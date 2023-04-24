cbac2a1d41632dda8e5f764ee88dcb511e99ef17
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-24 16:17:18 +0600

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
+public class MyHashTable<K, V> {
+    private int size;
+    private int capacity;
+    private LinkedList<Entry<K, V>>[] table;
 
+    public MyHashTable() {
+        this.capacity = 16;
+        this.table = new LinkedList[capacity];
+    }
+}
