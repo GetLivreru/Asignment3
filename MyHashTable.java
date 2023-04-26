e08808bb8bdf8e7daa016915201072544fecd7aa
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-26 10:40:52 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,3 +1,35 @@
+import java.util.*;
+class MyTestingClass{
+    private int id;
+    public MyTestingClass(int id){
+        this.id = id;
+    }
+    // custom hashcode implementation
+    @Override 
+    public int hascode(){
+        int hash = 17;
+        hash = 31*hash+id;
+        return hash;
+    }
+        // equals method to be used for testing purposes
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MyTestingClass other = (MyTestingClass) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+}
+        
+
+
 public class MyHashTable<K, V> {
     private int size;
     private int capacity;
