763a2c4dd33b18c0dba2b795e10b4356804686e2
theadilet <110306539+GetLivreru@users.noreply.github.com>
2023-04-30 13:46:39 +0600

    add class student

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,52 +1,18 @@
-import java.util.*;
-class MyTestingClass{
-    private int id;
-    public MyTestingClass(int id){
-        this.id = id;
+import java.util.Random;
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
-    // custom hashcode implementation
-    @Override 
-    public int hascode(){
-        int hash = 17;
-        hash = 31*hash+id;
-        return hash;
+
+    public String getName() {
+        return name;
     }
-        // equals method to be used for testing purposes
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final MyTestingClass other = (MyTestingClass) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        return true;
+
+    public int getAge() {
+        return age;
     }
 }
-        
-
-
-public class MyHashTable<K, V> {
-    private int size;
-    private int capacity;
-    private LinkedList<Entry<K, V>>[] table;
-
-    public MyHashTable() {
-        this.capacity = 16;
-        this.table = new LinkedList[capacity];
-    }
-    private static class Entry<K, V> {
-        K key;
-        V value;
-
-        Entry(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        // getters and setters omitted for brevity
- }
